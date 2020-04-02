0344ef2182c469f1a50d1b3f9715af8921696d1e
Juan José Torres <juanjtorres@pm.me>
2020-04-02 11:58:06 -0500

    Servidor y Cliente UDP terminados

--- src/UDP/Client.go
+++ src/UDP/Client.go
@@ -1,53 +1,155 @@
-package main
+package UDP
 
 import (
-	"bufio"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
-func main() {
-	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("Please provide a host:port string")
-		return
-	}
-	CONNECT := arguments[1]
+// Se declara en ambos archivos ya que correran en maquinas diferentes
+const BUFFERSIZE = 4048
 
-	s, err := net.ResolveUDPAddr("udp4", CONNECT)
-	c, err := net.DialUDP("udp4", nil, s)
+func verificarIntegridad(ruta, hashRecibido string, logger *log.Logger) bool {
+
+	// Crea una interfaz para realizar el hash
+	hash := md5.New()
+
+	archivo, err := os.Open(ruta)
+	if err != nil {
+		logger.Println("Hubo un problema al abrir el archivo para calcular su hash")
+		return false
+	}
+
+	defer archivo.Close()
+
+	// Copia el archivo a la interfaz
+	_, err = io.Copy(hash, archivo)
+	if err != nil {
+		logger.Println("Hubo un problema al copiar el archivo para calcular su hash")
+		return false
+	}
+
+	// Obtiene el hash en 16 bytes
+	hashInBytes := hash.Sum(nil)[:16]
+	hashCalculado := hex.EncodeToString(hashInBytes)
+	logger.Println("Se calculó el siguiente hash para el archivo recibido ", hashCalculado)
+	// Convierte el hash a String
+	if hashCalculado != hashRecibido {
+		logger.Println("El hash no se pudo verificar")
+		return false
+	}
+
+	logger.Println("Se verificó el hash del archivo de forma correcta")
+	return true
+}
+
+func descargar(dstFile string, conn *net.UDPConn, logger *log.Logger) bool {
+
+	bufferFileSize := make([]byte, 10)
+	conn.Read(bufferFileSize)
+	tamanio, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
+
+	logger.Println("Se recibirá un archivo de tamaño ", tamanio)
+
+	// Crea el archivo para luego guardar en este lo que se descarga
+	archivo, err := os.Create(dstFile)
+	if err != nil {
+		logger.Println("Hubo un error al crear el archivo  donde se descargará: ", err)
+		return false
+	}
+
+	antes := time.Now()
+	var bytes int64
+	i := 0
+	for {
+		conn.SetReadDeadline(time.Now().Add(10 * time.Second)) // Crea un límite de tiempo por si se cae la conexión
+
+		if (tamanio - bytes) < BUFFERSIZE {
+			_, err = io.CopyN(archivo, conn, tamanio-bytes)
+			if err != nil {
+				logger.Println("Hubo un error con la conexión y no se recibió el archivo completo")
+				fmt.Fprintln(conn, "No")
+				return false
+			}
+			i++
+			break
+		}
+		_, err := io.CopyN(archivo, conn, BUFFERSIZE)
+		if err != nil {
+			logger.Println("Hubo un error con la conexión y no se recibió el archivo completo")
+			fmt.Fprintln(conn, "No")
+			return false
+		}
+		i++
+		bytes += BUFFERSIZE
+	}
+	despues := time.Now()
+	archivo.Close()
+
+	logger.Println("Se recibieron ", i, " paquetes")
+	logger.Println("Se recibió un archivo (sin verificar) y tardó ", despues.Sub(antes).Seconds(), " segundos")
+
+	_, err = fmt.Fprintln(conn, "OK")
+	if err != nil {
+		logger.Println("Hubo un error al enviar el mensaje de control", err)
+		return false
+	}
+	// Obtiene el hash del servidor
+	buff := make([]byte, BUFFERSIZE)
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second)) // Crea un límite de tiempo por si se cae la conexión
+	tam, err := conn.Read(buff)
+	if err != nil {
+		logger.Println("Hubo un error al leer el hash del archivo", err)
+		return false
+	}
+
+	hash := string(buff[0:tam])
+	logger.Println("Se recibió el siguiente hash ", hash)
+	return verificarIntegridad(dstFile, strings.TrimSuffix(hash, "\n"), logger)
+}
+
+func main() {
+
+	logFile, err := os.Create("data/logClient.txt")
+	logger := log.New(logFile, ">>", log.LstdFlags)
+	logger.Println("Inicio")
+
+	fmt.Println("Indica la direccion:puerto a la cual desea conectarse")
+	var puerto string
+	fmt.Scanln(&puerto)
+
+	s, err := net.ResolveUDPAddr("udp4", puerto)
+	con, err := net.DialUDP("udp4", nil, s)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	l := con.LocalAddr()
+	logger.Println("Se estableció una conexión la dirección local es ", l, " la dirección remota es ", s)
 
-	fmt.Printf("The UDP server is %s\n", c.RemoteAddr().String())
-	defer c.Close()
+	defer con.Close()
 
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(">> ")
-		text, _ := reader.ReadString('\n')
-		data := []byte(text + "\n")
-		_, err = c.Write(data)
-		if strings.TrimSpace(string(data)) == "STOP" {
-			fmt.Println("Exiting UDP client!")
-			return
-		}
+	con.Write([]byte("Listo"))
+	//fmt.Fprintln(con, "Listo")
+	logger.Println("Se envió el mensaje de listo al servidor UDP")
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	antes := time.Now()
+	seDescargo := descargar("data/archivo.mp4", con, logger)
+	despues := time.Now()
 
-		buffer := make([]byte, 1024)
-		n, _, err := c.ReadFromUDP(buffer)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Printf("Reply: %s\n", string(buffer[0:n]))
+	if seDescargo != true {
+		con.Write([]byte("Error"))
+		logger.Println("Hubo un error descargando el archivo desde el servidor " + puerto)
+	} else {
+		seg := despues.Sub(antes).Seconds()
+		con.Write([]byte("Verificado"))
+		logger.Println("Se descargó y verificó correctamente el archivo y tardó ", seg, " segundos")
 	}
 }
--- src/UDP/Server.go
+++ src/UDP/Server.go
@@ -1,58 +1,205 @@
-package main
+package UDP
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
-	"math/rand"
+	"io"
+	"log"
 	"net"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
-func random(min, max int) int {
-	return rand.Intn(max-min) + min
+var logger *log.Logger
+// Se declara en ambos archivos ya que correran en maquinas diferentes
+const BUFFERSIZE = 4048
+
+func crearHash(archivo *os.File) string {
+
+	// Crea una interfaz para realizar el hash
+	hash := md5.New()
+
+	// Copia el archivo a la interfaz
+	_, err := io.Copy(hash, archivo)
+	if err != nil {
+		return ""
+	}
+
+	// Obtiene el hash en 16 bytes
+	hashInBytes := hash.Sum(nil)[:16]
+
+	// Convierte el hash a String
+	return hex.EncodeToString(hashInBytes)
 }
 
-func main() {
-	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("Please provide a port number!")
-		return
-	}
-	PORT := ":" + arguments[1]
-
-	s, err := net.ResolveUDPAddr("udp4", PORT)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	connection, err := net.ListenUDP("udp4", s)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer connection.Close()
-	buffer := make([]byte, 1024)
-	rand.Seed(time.Now().Unix())
-
+func completarString(retorno string, largo int) string {
 	for {
-		n, addr, err := connection.ReadFromUDP(buffer)
-		fmt.Print("-> ", string(buffer[0:n-1]))
-
-		if strings.TrimSpace(string(buffer[0:n])) == "STOP" {
-			fmt.Println("Exiting UDP server!")
-			return
+		largoRetorno := len(retorno)
+		if largoRetorno < largo {
+			retorno = retorno + ":"
+			continue
 		}
-
-		data := []byte(strconv.Itoa(random(1, 1001)))
-		fmt.Printf("data: %s\n", string(data))
-		_, err = connection.WriteToUDP(data, addr)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+		break
 	}
+	return retorno
+}
+
+func enviarArchivo(conn *net.UDPConn, add net.Addr, srcFile string, nCLiente int) bool {
+
+	defer conn.Close()
+
+	// Abre el archivo a enviar
+	arch, err := os.Open(srcFile)
+	if err != nil {
+		logger.Println("Hubo un error: ", err, " al abrir el archivo para enviar al cliente ", nCLiente)
+		return false
+	}
+	defer arch.Close()
+
+	logger.Println("Se abrió el archivo y se va a calcular su hash")
+
+	hash := crearHash(arch)
+
+	if hash != "" {
+
+		logger.Println("Se calculó el hash del archivo y es: ", hash)
+		// Abre de nuevo el archivo a enviar
+		arch, _ = os.Open(srcFile)
+
+		// Envía el archivo al cliente
+		info, err := arch.Stat()
+		if err != nil {
+			logger.Println("Hubo un error al sacar el tamaño del archivo ", err)
+			return false
+		}
+		// Esto se usa para llenar el buffer del cliente y que este no se quede esperando
+		tamanio := completarString(strconv.FormatInt(info.Size(), 10), 10)
+		antes := time.Now()
+		conn.WriteTo([]byte(tamanio), add)
+		buffer := make([]byte, BUFFERSIZE)
+		logger.Println("Empieza envío de archivo")
+		i := 0
+		for {
+			_, err := arch.Read(buffer)
+			i++
+			if err == io.EOF {
+				break
+			}
+			conn.WriteTo(buffer, add)
+		}
+
+		despues := time.Now()
+		seg := fmt.Sprintf("%f", despues.Sub(antes).Seconds())
+		logger.Println("Se enviaron ", i, " paquetes")
+		logger.Println("Se envió el archivo al cliente ", strconv.Itoa(nCLiente), "y tardó ", seg, " segundos")
+
+		buffIn := make([]byte, BUFFERSIZE)
+		n, _, err := conn.ReadFrom(buffIn)
+		msj0 := string(buffIn[0:n])
+		if err != nil {
+			logger.Println("Se produjo un error: ", err, "El cliente ", nCLiente, " no recibió el archivo")
+			return false
+		}
+		if msj0 == "OK" || msj0 == "OK\n" {
+			conn.WriteTo([]byte(hash), add)
+		} else {
+			logger.Println("El cliente ", nCLiente, " no recibe el archivo correctamente")
+			return false
+		}
+	} else {
+		logger.Println("Hubo un error al calcular el hash y enviarlo al cliente ", nCLiente)
+		return false
+	}
+
+	buffIn := make([]byte, BUFFERSIZE)
+	n, _, err := conn.ReadFrom(buffIn)
+	msj := string(buffIn[0:n])
+	msj3 := strings.TrimSuffix(msj, "\n")
+	if err != nil {
+		logger.Println("Se produjo un error: ", err, " y el cliente ", nCLiente, " no pudo verificar la integridad del archivo")
+		return false
+	} else if msj3 != "Verificado" {
+		logger.Println("El cliente ", nCLiente, " no pudo verificar la integridad del archivo")
+		return false
+	}
+
+	logger.Println("Se envió correctamente el archivo al cliente ", nCLiente)
+
+	return true
+}
+
+func iniciarConexiones(lAdd *net.UDPAddr, archivo string, i int, wg sync.WaitGroup) {
+
+	defer wg.Done()
+
+	conn, err := net.ListenUDP("udp", lAdd)
+	if err != nil {
+		logger.Println("Hubo un error al intentar establecer una conexión: ", err)
+		return
+	}
+
+	logger.Println("Se estableció una conexión")
+
+	defer conn.Close()
+
+	buffer := make([]byte, BUFFERSIZE)
+
+	n, add, err := conn.ReadFrom(buffer)
+	if err != nil {
+		logger.Println("Hubo un error al leer el mensaje de confirmación del cliente: ", err)
+		return
+	}
+	logger.Println("Se recibió un mensaje de ", add)
+
+	msj1 := string(buffer[0:n])
+	fmt.Println(msj1)
+
+	if msj1 == "Listo\n" || msj1 == "Listo" {
+		enviarArchivo(conn, add, archivo, i)
+	}
+
+}
+func main() {
+
+	logFile, _ := os.Create("data/logServer.txt")
+	logger = log.New(logFile, ">>", log.LstdFlags)
+	logger.Println("Inicio")
+
+	fmt.Println("Indica el puerto en  el que escuchar solicitudes")
+	var port string
+	fmt.Scanln(&port)
+	PORT := ":" + port
+
+	fmt.Println("Ingrese qué video quiere enviar:\n(1) video1.mp4\n(2) video2.mp4")
+	var seleccion string
+	var archivo string
+
+	fmt.Scanln(&seleccion)
+	if seleccion == "1" {
+		archivo = "data/video1.mp4"
+	} else {
+		archivo = "data/video2.mp4"
+	}
+
+	fmt.Println("Ingrese el número de usuarios que espera atender")
+	var numero int
+	fmt.Scanln(&numero)
+
+	var wg sync.WaitGroup
+	wg.Add(numero)
+
+	lAdd, err := net.ResolveUDPAddr("udp", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := 0; i < numero; i++ {
+		go iniciarConexiones(lAdd, archivo, i, wg)
+	}
+	wg.Wait()
 }
